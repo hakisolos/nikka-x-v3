0fecc14be6949fc71489fe3d32240ff987390b2a
hakisolos2 <excelmaxwell50@gmail.com>
2024-12-25 03:06:53 -0800

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,1 +1,6 @@
 //
+const axios = require("axios");
+const {API, APIKEY} = config;
+const yts = `${API}/search/yts?apiKey=${APIKEY}&q=`
+const ytmp3 = `${API}/dl/ytmp3?apiKey=${APIKEY}&url=`
+// and so on
