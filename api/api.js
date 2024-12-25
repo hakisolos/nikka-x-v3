1315a651d9d0f2bfb8c8c288b0a27c0a0527f296
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-12-25 12:16:22 +0100

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,6 +1,10 @@
-//
-const axios = require("axios");
-const {API, APIKEY} = config;
-const yts = `${API}/search/yts?apiKey=${APIKEY}&q=`
-const ytmp3 = `${API}/dl/ytmp3?apiKey=${APIKEY}&url=`
-// and so on
+const API = "https://nikka-api.us.kg";
+const KEY = "nikka"
+
+const endpoints = {
+    yts: (query) => `${API}/search/yts?apiKey=${APIKEY}&q=${encodeURIComponent(query)}`,
+    ytmp3: (url) => `${API}/dl/ytmp3?apiKey=${APIKEY}&url=${encodeURIComponent(url)}`,
+    
+};
+
+module.exports = api;
